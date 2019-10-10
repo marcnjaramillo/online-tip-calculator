0c05b0f1cc1f311f10d1961bd71ae2d0c0fb9a8e
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-10 00:35:55 -0700

    Create methods for handling the form data

--- src/TipCalculator.js
+++ src/TipCalculator.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const TipCalculator = () => {
-  return <div>Tip Calculator</div>;
+  const [tipFields, setTipFields] = useState({
+    bill: 0,
+    tip: 0,
+    party: 0
+  });
+
+  const handleChange = e => {
+    setTipFields({
+      ...tipFields,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  let totalTip = (tipFields.bill * tipFields.tip).toFixed(2);
 };
 
 export default TipCalculator;
